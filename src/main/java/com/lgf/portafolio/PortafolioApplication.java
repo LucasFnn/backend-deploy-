e166b48f5ca17bb35d5a636b5a12a47c1700e218
LucasFnn <123983687+LucasFnn@users.noreply.github.com>
2023-05-15 00:43:42 -0300

    Update PortafolioApplication.java

--- src/main/java/com/lgf/portafolio/PortafolioApplication.java
+++ src/main/java/com/lgf/portafolio/PortafolioApplication.java
@@ -12,6 +12,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class PortafolioApplication implements CommandLineRunner{
     
         @Autowired 
